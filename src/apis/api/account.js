47732b92605cccf01d30a985412a3844e33c08a2
mini3100 <mini3100@naver.com>
2023-09-26 15:43:15 +0900

    230926 수업

--- src/apis/api/account.js
+++ src/apis/api/account.js
@@ -3,4 +3,11 @@ import instance from "../utils/Instance"
 export const signup = async (account) => {
     const response = await instance.post("/api/v1/auth/user", account);
     console.log(response);
+    return response;
+}
+
+export const signin = async (account) => {
+    const response = await instance.post("/api/v1/auth/login", account);
+    console.log(response);
+    return response;
 }
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from 'react-router-dom';
 function Signup(props) {
     const navigate = useNavigate();
     const emptyAccount = {
-        phoneAndEmail: "",
+        phoneOrEmail: "",
         name: "",
         username: "",
         password: ""
@@ -39,8 +39,8 @@ function Signup(props) {
             const keys = Object.keys(responseErrorMsg);  // 객체의 키값들만 리스트로 빼줌.
             if(keys.includes("username")) {
                 setErrorMsg(responseErrorMsg.username);
-            } else if(keys.includes("phoneAndEmail")) {
-                setErrorMsg(responseErrorMsg.phoneAndEmail);
+            } else if(keys.includes("phoneOrEmail")) {
+                setErrorMsg(responseErrorMsg.phoneOrEmail);
             } else if(keys.includes("name")) {
                 setErrorMsg(responseErrorMsg.name);
             } else if(keys.includes("password")) {
@@ -61,7 +61,7 @@ function Signup(props) {
                         Kakao로 로그인
                     </button>
                     <OrBar/>
-                    <Input placeholder={"휴대폰 번호 또는 이메일 주소"} name={"phoneAndEmail"} changeAccount={changeAccount}/>
+                    <Input placeholder={"휴대폰 번호 또는 이메일 주소"} name={"phoneOrEmail"} changeAccount={changeAccount}/>
                     <Input placeholder={"성명"} name={"name"} changeAccount={changeAccount}/>
                     <Input placeholder={"사용자 이름"} name={"username"} changeAccount={changeAccount}/>
                     <Input type={"password"} placeholder={"비밀번호"} name={"password"} changeAccount={changeAccount}/>
